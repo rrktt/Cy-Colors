60b9aadc3b23f97826106c38b45ef30fd903329e
rrkt <131724382+rrktt@users.noreply.github.com>
2023-05-16 00:38:25 +0200

    Create ini.h

---
+++ ini.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include<unistd.h>
+#define ROWS 8
+#define COWS 8
+#include "struct.h"
+#include "valcoul.h"
+int* type();
+void initialisation(Pion colors[ROWS][ROWS]);
