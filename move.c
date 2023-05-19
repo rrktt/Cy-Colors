d4779a8dc6bc4e3a8c44a29af3a875006bd9fb6b
raide3 <130498474+raide3@users.noreply.github.com>
2023-05-19 23:02:28 +0200

    Update move.c

--- move.c
+++ move.c
@@ -13,11 +13,11 @@ do{
   printf("entrer les nouvelles coordonnées :\n ");
   scanf("%d %d", &a, &b);
   
-    printf( "\033[0;0H\033[2J");
-      colors[a][b].p=colors[x][y].p;
-     colors[x][y].p=16;
+    printf( "\033[0;0H\033[2J");// permet d'effacer le contenu du terminal¿
+      colors[a][b].p=colors[x][y].p;// remplace la valeur de la deuxième case sélèctionner par celle de la première 
+     colors[x][y].p=16;//remplace la valeur de la première case par un vide
   affiche(colors);
-    }while( x<0 || x>=8 || y<0 || y>=8 || colors[x][y].p==16 );
+    }while( x<0 || x>=8 || y<0 || y>=8 || colors[x][y].p==16 );//incomplet 
 }
-while(colors[a][b].p>16 && colors[a][b].p<=8 || colors[a][b].p>8 && colors[a][b].p<0);
+while(colors[a][b].p>16 && colors[a][b].p<=8 || colors[a][b].p>8 && colors[a][b].p<0);//incomplet boucle infinie
 }
