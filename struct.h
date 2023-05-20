29f7ab69646d0678af4341b15fa19e4cff217f46
raide3 <130498474+raide3@users.noreply.github.com>
2023-05-20 14:14:29 +0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -5,7 +5,7 @@ typedef struct{
 int color ;// utilité non démontré
 int background ;
 bool promoted ;// true = promue, false = non promue
-char type[7];//contient l'élément d'une case
+char type[13];//contient l'élément d'une case
 int abs ;// utilité non démontré
 int ord ;// utilité non démontré
 int p;// indique si une case doit être vide ou doit contenir l' un des deux pions
