929f03972147b3294d53d0ba5a5cadd035ff8997
raide3 <130498474+raide3@users.noreply.github.com>
2023-05-22 09:40:07 +0200

    Update move.c

--- move.c
+++ move.c
@@ -1,26 +1,55 @@
 #include"move.h"
 //#include"prere.h"
 #include<stdio.h>
-void vide_buffer(){
-  while(getchar()!='\n'){}
-}
-void move(Pion colors[ROWS][ROWS]){
-  int x, y, a, b, verif, verif0;
-  
-do{
-  
+
+void move_total(int *a1, int* a2, Pion colors[ROWS][ROWS]){
+int a,b,f,g, b2;
+int v=1;
   do{
-      printf("\nentrer les coordonnées du pion à déplacer : \n");
-    verif=scanf("%d %d", &x, &y);
-    vide_buffer();
-  printf("entrer les nouvelles coordonnées :\n ");
-    verif0=scanf("%d %d", &a, &b);
-    vide_buffer();
-    printf( "\033[0;0H\033[2J");// permet d'effacer le contenu du terminal¿
-      colors[a][b].p=colors[x][y].p;// remplace la valeur de la deuxième case sélèctionner par celle de la première 
-     colors[x][y].p=16;//remplace la valeur de la première case par un vide
+
+    printf("\nentrer les nouvelles coordonnées \n");
+  scanf("%d %d", &a, &b);
+  for(int i=0; i<ROWS; i++){
+    for(int j=0; j<ROWS; j++){
+    if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<16 && colors[i][j].p>7){
+      colors[a][b].p=colors[i][j].p;
+     colors[i][j].p=16;
   affiche(colors);
-    }while( x<0 || x>=8 || y<0 || y>=8 || colors[x][y].p==16 );//incomplet 
+    } 
+    }
+  }*a1=a; *a2=b;
+    /*    printf("\nentrer les nouvelles coordonnées \n");
+  scanf("%d %d", &f, &g);
+  for(int i=0; i<ROWS; i++){
+    for(int j=0; j<ROWS; j++){
+    if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<8 && colors[i][j].p>=0){
+      colors[f][g].p=colors[i][j].p;
+     colors[i][j].p=16;
+  affiche(colors);
+    }
+    }  }
+    *a1=f; *a2=g*/
+  }
+while(1/*colors[a][b].p>16 && colors[a][b].p<=8 || colors[a][b].p>8 && colors[a][b].p<0*/);
 }
-while(colors[a][b].p>16 && colors[a][b].p<=8 || colors[a][b].p>8 && colors[a][b].p<0);//incomplet boucle infinie
+  
+void move( Pion colors[ROWS][ROWS]){
+ int a, b;
+  int x,y;
+  printf("\nentrer les coordonnées du       pion à déplacer : \n");
+    scanf("%d %d", &x, &y);
+    
+  printf("entrer les nouvelles coordonnées ");
+  scanf("%d %d", &a, &b);
+  
+    printf( "\033[0;0H\033[2J");
+      colors[a][b].p=colors[x][y].p;
+     colors[x][y].p=16;
+  affiche(colors);
+
+  move_total(&a, &b, colors);
+
+
 }
+
+
