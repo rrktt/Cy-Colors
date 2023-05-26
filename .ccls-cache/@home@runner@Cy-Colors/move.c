a9a4893882eb21a1ca29211f0bae504b0abbc7fe
rrktt <131724382+rrktt@users.noreply.github.com>
2023-05-26 21:00:37 +0000

    ça devrait remarcher normalement

--- .ccls-cache/@home@runner@Cy-Colors/move.c
+++ .ccls-cache/@home@runner@Cy-Colors/move.c
@@ -1,12 +1,14 @@
-#include "move.h"
-//#include "prere.h"
+#include"move.h"
+//#include"prere.h"
+#include<stdio.h>
 
 void move_total(int *a1, int* a2, Pion colors[ROWS][ROWS]){
 int a,b,f,g, b2;
-int ap1= *a1,bp1= *a2,ap2,bp2; //Variables qui stockent les coordonées des tours précédents de chaque joueurs
-int v=1,player,turn =2;
+int v=1;
   do{
-    player= pow(-1,turn);//Permet de Savoir quel joueur joue
+
+    printf("\nentrer les nouvelles coordonnées \n");
+  scanf("%d %d", &a, &b);
   for(int i=0; i<ROWS; i++){
     for(int j=0; j<ROWS; j++){
     if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<16 && colors[i][j].p>7){
@@ -16,7 +18,6 @@ int v=1,player,turn =2;
     } 
     }
   }*a1=a; *a2=b;
-    ap1= a; bp1=b;
         printf("\nentrer les nouvelles coordonnées \n");
   scanf("%d %d", &f, &g);
   for(int i=0; i<ROWS; i++){
@@ -35,7 +36,7 @@ while(1/*colors[a][b].p>16 && colors[a][b].p<=8 || colors[a][b].p>8 && colors[a]
 void move( Pion colors[ROWS][ROWS]){
  int a, b;
   int x,y;
-  printf("\nentrer les coordonnées du       pion à déplacer : \n");
+   printf("\nTour du joueur 1\nentrer les coordonnées du       pion à déplacer : \n");
     scanf("%d %d", &x, &y);
     while(y!=0){
       printf("\nIl n'y a pas de pion ici!!\n");
@@ -56,7 +57,8 @@ void move( Pion colors[ROWS][ROWS]){
       printf("\nVous ne pouvez pas vous déplacer comme ceci!!!\n");
       scanf("%d %d",&a,&b);
       }
-      } 
+      }
+  
     printf( "\033[0;0H\033[2J");
       colors[a][b].p=colors[x][y].p;
      colors[x][y].p=16;
@@ -66,5 +68,4 @@ void move( Pion colors[ROWS][ROWS]){
 
 
 }
-
 
