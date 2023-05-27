e0a26dcd00940668eb39414004305ad081316bb1
raide3 <130498474+raide3@users.noreply.github.com>
2023-05-27 23:26:23 +0200

    Update move.c

--- move.c
+++ move.c
@@ -3,60 +3,62 @@
 #include<stdio.h>
 
 void move_total(int *a1, int* a2, Pion colors[ROWS][ROWS]){
-int a,b,f, score, score1, g, b2, fin, cond, cond1;
-  do{
+  int a,b,f, score, score1, g, b2, fin, cond, cond1, psp;
   int score1=0,score2=0,manche = 0;
-int v=1,ap,bp,fp,gp, ver, ver1, block, block1, obs,obs1, fin;
-  
+  int v=1,ap,bp,fp,gp, ver, ver1, block, block1, obs,obs1, fin;
+    do{
+      
   for(int i=0; i<ROWS; i++){
     for(int j=0; j<ROWS; j++){
-    if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<16 && colors[i][j].p>7){
-      obs=check_obstacles(i, j, a, b,  colors);
-      
-      block=bloque1(&i,&j,  colors);
-        do{
-    printf("\nTour du joueur 2\nentrer les nouvelles coordonnées de (%d %d) \n", i, j);
-  ver=scanf("%d %d", &a, &b); 
-          cond=condition( i, j, a, b);
-          
+      if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<16 && colors[i][j].p>7){
+        obs=check_obstacles(i, j, a, b,  colors);
+        block=bloque1(&i,&j,  colors);
+          do{
+             printf("\nTour du joueur 2\nentrer les nouvelles coordonnées de (%d %d) \n", i, j);
+              ver=scanf("%d %d", &a, &b); 
+              cond=condition( i, j, a, b);
           }while(ver!=2 || cond==0);
-          choisirPion(&i, &j, &a, &b,  colors);
-      if(obs==0/*(colors[i-1][j-1].p != 16) && (colors[i][j-1].p !=16) && (colors[i+1][j-1].p != 16)*/){
-        printf("\n Vous ne pouvez pas vous déplacer\n");
-        b= bp; a=ap;
-      }
-      else {
-        conditions2(&a,&b,i,j);
+          
+            // choisirPion(&i, &j, &a, &b,  colors);
+           if(obs==0/*(colors[i-1][j-1].p != 16) && (colors[i][j-1].p !=16) && (colors[i+1][j-1].p != 16)*/){
+               printf("\n Vous ne pouvez pas vous déplacer\n");
+               b= bp; 
+               a=ap;
+            }
+            else {
+               conditions2(&a,&b,i,j);
       }
       /*if(a==0){
         printf("\nLe joueur 2 à remporter la manche\n");
         score2++;
         manche=1;*/
       
-      colors[a][b].p=colors[i][j].p;
-     colors[i][j].p=16;
-  affiche(colors);
-    } 
-    }
+          colors[a][b].p=colors[i][j].p;
+          colors[i][j].p=16;
+           affiche(colors);
+        } 
+     }
   }
-    *(a1)=a; *(a2)=b;
-    ap = a; bp =b;
+      *(a1)=a; 
+      *(a2)=b;
+      ap = a; 
+      bp =b;
   
   for(int i=0; i<ROWS; i++){
     for(int j=0; j<ROWS; j++){
-    if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<8 && colors[i][j].p>=0){
-        do{
-        printf("\nTout du joueur 1\nentrer les nouvelles coordonnées de (%d %d) \n", i, j);
-     ver1= scanf("%d %d", &f, &g);
-          vide_buffer();
-          cond1=condition1( i, j, f, g);
-    }while(ver1!=2 || cond==0);
+      if(colors[i][j].background==colors[*a1][*a2].background && colors[i][j].p<8 && colors[i][j].p>=0){
+         do{
+            printf("\nTout du joueur 1\nentrer les nouvelles coordonnées de (%d %d) \n", i, j);
+            ver1= scanf("%d %d", &f, &g);
+            vide_buffer();
+            cond1=condition1( i, j, f, g);
+         }while(ver1!=2 || cond==0);
          block1=bloquer(&i, &j, colors);
-          obs=check_obstacles(i, j, f, g,  colors);
-      choisirPion(&i, &j, &f, &g,  colors);
-       if(obs==0/*(colors[i-1][j+1].p != 16) && (colors[i][j+i].p !=16) && (colors[i+1][j+1].p != 16)*/){
+         obs=check_obstacles(i, j, f, g,  colors);
+         choisirPion(&i, &j, &f, &g,  colors);
+         if(obs==0/*(colors[i-1][j+1].p != 16) && (colors[i][j+i].p !=16) && (colors[i+1][j+1].p != 16)*/){
         
-        printf("\n Vous ne pouvez pas vous déplacer\n");
+         printf("\n Vous ne pouvez pas vous déplacer\n");
       }
       else {
       conditions1(&f,&g,i,j );
@@ -66,12 +68,15 @@ int v=1,ap,bp,fp,gp, ver, ver1, block, block1, obs,obs1, fin;
         score1++;
         manche=1;*/
       colors[f][g].p=colors[i][j].p;
-     colors[i][j].p=16;
-  affiche(colors);
+      colors[i][j].p=16;
+      affiche(colors);
     }
-    }  }
-    *a1=f; *a2=g;
-  } while (fin == 3 || fin == 2);
+   }  
+ }
+      *a1=f; 
+      *a2=g;
+      fin=fin_de_partie(score, score1, block, block1,  psp, color)
+} while (fin == 3 || fin == 2);
 if(fin==3){
   printf("c'est le joueur 2 qui gagne %d", score);
   
