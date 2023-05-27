6641e00cf27f344727a38ecdb44bb331cb88c54f
raide3 <130498474+raide3@users.noreply.github.com>
2023-05-27 17:56:39 +0200

    Update conditions.h

--- conditions.h
+++ conditions.h
@@ -4,3 +4,9 @@
 void conditions2(int* a,int*b, int i, int j );
 void conditions1(int* f,int*g, int i, int j );
 void conditionsStart(int* x,int*y, int i, int j );
+void choisirPion(int* x, int* y, int*a, int *b, Pion colors[ROWS][ROWS]); 
+void vide_buffer();
+int bloquer( int* posx2, int* posy2, Pion color[ROWS][ROWS]);
+int bloque1( int* posx, int* posy2, Pion color [ROWS][ROWS]);
+int fin_de_partie(int* score1, int* score2, int *rec, int* rec2, int* psp, Pion color[ROWS][ROWS]);
+int check_obstacles(int x1, int y1, int x2, int y2, Pion board[ROWS][ROWS]); 
